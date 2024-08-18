3c08e2a07378b5e7833b6eeb78a305929de7164d
Jose Lugo <josealr97@gmail.com>
2024-08-18 17:24:59 -0500

    Hook system connection

--- Assets/__Scripts/HookSystem.cs
+++ Assets/__Scripts/HookSystem.cs
@@ -1,43 +1,81 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class HookSystem : MonoBehaviour
 {
     //Components
-    [SerializeField] private GameObject HookPrefab;
-
+    [SerializeField] private GameObject hookPrefab;
+    [SerializeField] private DistanceJoint2D joint;
+    
     //Balance Variables
-    [SerializeField] private float launchSpeed = 50;
-    [SerializeField] private float maxDistance = 10;
-    [SerializeField] private float cooldown = 5;
+    [SerializeField] private float launchSpeed = 25;
+    [SerializeField] private float maxDistance = 6;
+    [SerializeField] private float cooldown = 3;
     [SerializeField] private float ammo = 3;
     
     //Work Variables
-    private bool canShoot = true;
+    private bool _canShoot = true;
+    private bool _hooked = false;
     
     //Functions
     private void LaunchHook()
     {
-        GameObject hook = Instantiate(HookPrefab, transform.position, transform.rotation);
+        GameObject hook = Instantiate(hookPrefab, transform.position, transform.rotation);
         hook.GetComponent<Rigidbody2D>().velocity = hook.transform.up * launchSpeed;
     }
 
-    private void Update()
+    public void Connect(Rigidbody2D hit)
     {
-        if (Input.GetKeyDown(KeyCode.F) && canShoot)
+        if (_hooked) //no more than one connection
+            return;
+
+        if ((hit.position - (Vector2)transform.position).magnitude > maxDistance)
         {
-            LaunchHook();
-            canShoot = false;
-            StartCoroutine(Cooldown());
+            Debug.Log("Debris too far");
+            return;
         }
+        
+        joint.enabled = true;
+        joint.connectedBody = hit;
+        joint.distance = maxDistance;
+        _hooked = true;
+    }
+
+    private void RenderRope()
+    {
+        
     }
     
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (_canShoot && !_hooked)
+            {
+                LaunchHook();
+                _canShoot = false;
+                StartCoroutine(Cooldown());
+            }else if (_hooked)
+            {
+                Unhook();
+            }
+            
+        }
+    }
+
+    private void Unhook()
+    {   
+        Debug.Log("Unhook()");
+    }
+
     IEnumerator Cooldown()
     {
        yield return new WaitForSeconds(cooldown);
-       canShoot = true;
+       _canShoot = true;
     }
     
 }
